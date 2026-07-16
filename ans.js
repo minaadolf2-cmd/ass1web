31b1b39a0efa1d4b565db62a478bfe9a7e427c13
minaadolf2-cmd <minaadolf2@gmail.com>
2026-07-16 16:34:40 +0300

    Add files via upload

--- ans.js
+++ ans.js
@@ -1,53 +1,39 @@
-let studentName = "Mina";
-let attendance = 85;
-let midtermScore = 25;
-let finalExamScore = 50;
-let assignmentScore = 18;
-let tuitionPaid = true;
+function isValid(s) {
 
-if (tuitionPaid === false) {
+    let stack = [];
 
-    console.log("You cannot view your results because your tuition fees are unpaid.");
+    for (let i = 0; i < s.length; i++) {
 
-} else {
+        if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
 
-    if (attendance < 75) {
+            stack.push(s[i]);
 
-        console.log("Student: " + studentName);
-        console.log("Status: Failed due to low attendance.");
-
-    } else {
-
-        let totalScore = midtermScore + finalExamScore + assignmentScore;
-        let letterGrade = "";
-
-        if (totalScore >= 90) {
-            letterGrade = "A";
-        } else if (totalScore >= 80) {
-            letterGrade = "B";
-        } else if (totalScore >= 70) {
-            letterGrade = "C";
-        } else if (totalScore >= 60) {
-            letterGrade = "D";
         } else {
-            letterGrade = "F";
-        }
 
-        console.log("Student Name: " + studentName);
-        console.log("Attendance: " + attendance + "%");
-        console.log("Total Score: " + totalScore);
-        console.log("Letter Grade: " + letterGrade);
+            let last = stack.pop();
 
-        if (letterGrade === "F") {
-            console.log("Academic Status: Failed");
-        } else {
-            console.log("Academic Status: Passed");
-        }
+            if (s[i] === ")" && last !== "(") {
+                return false;
+            }
 
-        if (letterGrade === "A") {
-            console.log("Congratulations! You are eligible for a scholarship.");
+            if (s[i] === "]" && last !== "[") {
+                return false;
+            }
+
+            if (s[i] === "}" && last !== "{") {
+                return false;
+            }
+
         }
 
     }
 
+    return stack.length === 0;
+
 }
+
+console.log(isValid("()"));
+console.log(isValid("()[]{}"));
+console.log(isValid("(]"));
+console.log(isValid("([)]"));
+console.log(isValid("{[]}"));
